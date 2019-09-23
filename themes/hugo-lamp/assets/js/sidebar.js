f6dd6109ac593dfa3926277088a7d8179ff21339
chabad360 <chabad360@gmail.com>
2019-09-23 02:47:32 +0000

    serving commento localy

--- themes/hugo-lamp/assets/js/sidebar.js
+++ themes/hugo-lamp/assets/js/sidebar.js
@@ -1,4 +1,4 @@
-
+/*
 function openSidebar() {
   var sidebar = document.getElementById("sidebar");
   var mask = document.getElementById("sidebar-mask");
@@ -14,3 +14,6 @@ function closeSidebar() {
   mask.removeAttribute("open", "");
   mask.setAttribute("hidden", "");
 }
+*/
+
+function openSidebar(){var e=document.getElementById("sidebar"),t=document.getElementById("sidebar-mask");e.setAttribute("open",""),t.removeAttribute("hidden",""),t.setAttribute("open","")}function closeSidebar(){var e=document.getElementById("sidebar"),t=document.getElementById("sidebar-mask");e.removeAttribute("open",""),t.removeAttribute("open",""),t.setAttribute("hidden","")}
