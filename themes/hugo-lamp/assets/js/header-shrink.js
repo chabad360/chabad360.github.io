70676394e38ee218ca54b5e75f4d7ca7d409b262
chabad360 <chabad360@gmail.com>
2019-11-05 05:10:58 +0000

    fixed copy-btn added header shrink on mobile, etc.

---
+++ themes/hugo-lamp/assets/js/header-shrink.js
@@ -0,0 +1,18 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if(window.innerWidth <= 800){
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+      document.getElementById("title").setAttribute("hidden", "true");
+      document.getElementById("subtitle").setAttribute("hidden", "true");
+      document.getElementById("social").setAttribute("hidden", "true");
+      document.getElementById("header").classList.add("header-shrunk");
+    } else {
+      document.getElementById("header").classList.remove("header-shrunk");
+      document.getElementById("title").removeAttribute("hidden", "");
+      document.getElementById("subtitle").removeAttribute("hidden", "");
+      document.getElementById("social").removeAttribute("hidden", "");
+    }
+  }
+}
